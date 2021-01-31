9d1d21b9db21ecd2f72ac27225152a7f8e1e05a4
nargok <nargok@gmail.com>
2021-01-31 12:57:42 +0900

    router for todo page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Home from "@/views/Home.vue";
 
 Vue.use(VueRouter);
 
@@ -18,6 +18,14 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/todo",
+    name: "Todo",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ "@/views/Todo.vue")
   }
 ];
 
---
+++ src/views/Todo.vue
@@ -0,0 +1,16 @@
+<template>
+  <el-container>
+    <h1>Todo {{ msg }}</h1>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: "Todo",
+  data() {
+    return {
+      msg: "hello"
+    };
+  }
+};
+</script>
