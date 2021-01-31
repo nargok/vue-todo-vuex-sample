e66516ea8f4b23229471e7b616f7fb7ac978c19b
nargok <nargok@gmail.com>
2021-01-31 14:10:33 +0900

    task form

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -2,6 +2,14 @@
   <el-container>
     <el-main>
       <h1>Todo {{ msg }}</h1>
+      <el-form :inline="true" label-width="120px">
+        <el-form-item>
+          <el-input placeholder="タスクを入力" v-model="todo"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="createTask">ボタン</el-button>
+        </el-form-item>
+      </el-form>
       <TodoList listTitle="Todo" :todos="todos" :taskStatus="false" />
       <TodoList listTitle="Done" :todos="todos" :taskStatus="true" />
     </el-main>
@@ -25,6 +33,7 @@ export default {
   data() {
     return {
       msg: "hello",
+      todo: "",
       todos: [
         {
           id: 1,
@@ -45,8 +54,14 @@ export default {
     };
   },
   methods: {
-    toggleTaskStatus(task) {
-      return !task.status;
+    createTask() {
+      const task = {
+        id: this.todos.length + 1,
+        title: this.todo,
+        done: false
+      };
+      this.todos.push(task);
+      this.todo = "";
     }
   }
 };
