a82805f4488a54c8b7fe40e6ec3c37627253aae0
nargok <nargok@gmail.com>
2021-01-31 13:06:21 +0900

    display task list

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,15 +1,45 @@
 <template>
   <el-container>
-    <h1>Todo {{ msg }}</h1>
+    <el-main>
+      <h1>Todo {{ msg }}</h1>
+      <ul>
+        <li v-for="todo in todos" :key="todo.id" :class="{ done: todo.done }">
+          {{ todo.title }}
+        </li>
+      </ul>
+    </el-main>
   </el-container>
 </template>
+
+<style>
+.done {
+  text-decoration: line-through;
+}
+</style>
 
 <script>
 export default {
   name: "Todo",
   data() {
     return {
-      msg: "hello"
+      msg: "hello",
+      todos: [
+        {
+          id: 1,
+          title: "これはタスク1",
+          done: false
+        },
+        {
+          id: 2,
+          title: "これはタスク2",
+          done: true
+        },
+        {
+          id: 2,
+          title: "これはタスク2",
+          done: false
+        }
+      ]
     };
   }
 };
